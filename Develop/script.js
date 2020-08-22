8840268ccbd373dd9075cbfb39621a0d37882512
Tom Chestnut <tchestnut85@gmail.com>
2020-08-22 16:41:57 -0400

    added variables and beginning of password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,26 @@
-// Assignment code here
+// generate random letters, numbers and characters
 
+// make prompts when clicking the GENERATE button
+  // prompt asking for length of password
+  // prompt for character types to include in password choosing from lowercase, uppercase, numeric and/or special chars
+  // validate user input and at least one character type should be seclected
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var lettersLow = "abcdefghijklmnopqrstuvwxyz";
+var lettersUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var special = "!#$%&'()*+,-./:;<=>?@[]/^_`{}~";
+
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+function writePassword {
 
 }
+
+  // make if statements based on user input from confirms, and at least 1 confirm must be true
+  
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
